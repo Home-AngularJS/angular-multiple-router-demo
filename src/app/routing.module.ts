57ebba8977206f81d3831212726ad37f84b9377e
Lazarchuk <a.lazarchuk@cartsys.com.ua>
2019-11-21 18:11:54 +0200

    #angular7_8-multiple-router-demo

--- src/app/routing.module.ts
+++ src/app/routing.module.ts
@@ -9,11 +9,11 @@ import { ProductDetailComponent } from './layout/product-detail/product-detail.c
 import { HomeComponent } from './layout/home/home.component';
 
 const routes: Routes = [
-  { path: "", component: DefaultOutletComponent, outlet: "outlet" }, // устанавливаем этот компонент для маршрутизатора ПО УМОЛЧАНИЮ
-  { path: "1", component: Outlet1Component, outlet: "outlet" }, // устанавливаем этот компонент ДЛЯ ПЕРВОГО маршрутизатора
-  { path: "2", component: Outlet2Component, outlet: "outlet" }, // устанавливаем этот компонент ДЛЯ ВТОРОГО маршрутизатора
-  { path: "home", component: HomeComponent },                /*   http://localhost:4200/home   http://localhost:4200/home(outlet:1)   http://localhost:4200/home(outlet:2)   */
-  { path: "products", component: ProductListComponent },     /*   http://localhost:4200/products   http://localhost:4200/products(outlet:1)   http://localhost:4200/products(outlet:2)   */
-  { path: "product/:id", component: ProductDetailComponent } /*   http://localhost:4200/product/1   http://localhost:4200/product/1(outlet:1)   http://localhost:4200/product/1(outlet:2)   */
+  { path: '', component: DefaultOutletComponent, outlet: 'outlet' }, // устанавливаем этот компонент для маршрутизатора ПО УМОЛЧАНИЮ
+  { path: '1', component: Outlet1Component, outlet: 'outlet' }, // устанавливаем этот компонент ДЛЯ ПЕРВОГО маршрутизатора
+  { path: '2', component: Outlet2Component, outlet: 'outlet' }, // устанавливаем этот компонент ДЛЯ ВТОРОГО маршрутизатора
+  { path: 'home', component: HomeComponent },                /*   http://localhost:4200/home   http://localhost:4200/home(outlet:1)   http://localhost:4200/home(outlet:2)   */
+  { path: 'products', component: ProductListComponent },     /*   http://localhost:4200/products   http://localhost:4200/products(outlet:1)   http://localhost:4200/products(outlet:2)   */
+  { path: 'product/:id', component: ProductDetailComponent } /*   http://localhost:4200/product/1   http://localhost:4200/product/1(outlet:1)   http://localhost:4200/product/1(outlet:2)   */
 ];
 export const routingModule: ModuleWithProviders = RouterModule.forRoot(routes);
