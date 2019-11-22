7dcdf4b2dfd893b62d912d9f0422610f961f29d5
Lazarchuk <a.lazarchuk@cartsys.com.ua>
2019-11-22 14:35:58 +0200

    #master

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -12,7 +12,7 @@ const adminRoutes: Routes = [
   {
     path: '',
     component: AdminComponent,
-    canActivate: [AuthGuard],
+    // canActivate: [AuthGuard],
     children: [
       {
         path: '',
